66d50317eab2eb6bc384a0e96419bb84df2e6112
ecoiners <damuna63@gmail.com>
2025-09-23 04:45:03 +0700

    fixk

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-		react(),
-		tailwindcss(),
-	],
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    host: true, // biar bisa diakses di Replit/VM luar
+  },
+  build: {
+    outDir: "dist", // folder hasil build
+  }
 })

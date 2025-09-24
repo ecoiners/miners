676610f30a5548df3896163b858bf2b30831938c
ecoiners <damuna63@gmail.com>
2025-09-25 06:48:04 +0700

    fix news

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function AuthLayout({ children }) {
 	
 	return (
-		<div className="flex pt-48 justify-center">
+		<div className="flex mt-48 justify-center">
 		  {children}
 		</div>
 	);

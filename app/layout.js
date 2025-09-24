0d535e7db95c53139f524759d1941816a1e0e0d4
ecoiners <damuna63@gmail.com>
2025-09-24 19:51:10 +0700

    fix layout 4

--- app/layout.js
+++ app/layout.js
@@ -26,8 +26,8 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
 				>
-				  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY>
-						<ConvexClientProvider}>
+				  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+						<ConvexClientProvider>
 						  <main className="bg-slate-900 text-white overflow-x-hidden min-h-screen">
                 {children}
 							</main>
@@ -38,3 +38,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+

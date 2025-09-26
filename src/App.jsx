13d647ae55399db9325b52f15890d3e4c65d4d3f
ecoiners <damuna63@gmail.com>
2025-09-27 03:57:37 +0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screen/home";
 import Earn from "./screen/earn";
 import Dayli from "./screen/dayli";
@@ -18,7 +18,7 @@ function App() {
 				<Route path="/invite" element={<Refferals />} />
 				<Route path="/wallet" element={<Wallet />} />
 				
-				<Route path="*" element={<Navigate to="/" replace />} />
+				
 			</Routes>
     </Router>
   )

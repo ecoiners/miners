2aafef479a10e6fac7452169d6f519e036520298
ecoiners <damuna63@gmail.com>
2025-09-28 16:44:20 +0700

    src/rtk/api/BaseQuery.jsx

--- src/rtk/api/BaseQuery.jsx
+++ src/rtk/api/BaseQuery.jsx
@@ -1,13 +1,25 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+
+
 const BaseQuery = createApi({
+
     reducerPath: "API",
+
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8080/api/v1",
+
+        baseUrl: "https://backend-test-pearl-zeta.vercel.app/api/v1",
+
         // baseUrl: "https://hyperbull-api.syntax-siam.xyz/api/v1",
+
         // baseUrl: "https://hyper-bull-server.vercel.app/api/v1",
+
     }),
+
     endpoints: () => ({})
+
 });
+
+
 
 export default BaseQuery;

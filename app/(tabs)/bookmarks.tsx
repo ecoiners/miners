105d81cee685f7eda1c6a1be5e38d6e203ad6b9a
ecoiners <damuna63@gmail.com>
2025-10-07 08:52:41 +0700

    main update

---
+++ app/(tabs)/bookmarks.tsx
@@ -0,0 +1,10 @@
+import {View} from "react-native";
+
+export default function Bookmarks() {
+	
+	return (
+		<View>
+		  bookmarks
+		</View>
+	);
+};
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,40 @@
-import { Stack } from "expo-router";
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import "../global.css";
+import { useFonts } from "expo-font";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SplashScreen } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
+import { useCallback, useEffect } from "react";
+import * as NavigationBar from "expo-navigation-bar";
+import ClerkAndConvexProvider from "@/components/clerk-convex-provider";
+import InitialLayout from "@/components/initial-layout";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+	const [fontsLoaded] = useFonts({
+		"JetBrainsMono-Medium":require("../assets/fonts/JetBrainsMono-Medium.ttf")
+	});
+	
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) await SplashScreen.hideAsync();
+	}, [fontsLoaded]);
+	
+	  // update the native navigation bar on Android.
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync("#000000");
+      NavigationBar.setButtonStyleAsync("light");
+    }
+  }, []);
 	
 	return (
-		<ClerkProvider tokenCache={tokenCache}>
-		  <Stack>
-			  <Stack name="(auth)" options={{headerShown: false}} />
-				
-			</Stack>
-		</ClerkProvider>
+		<ClerkAndConvexProvider>
+		  <SafeAreaProvider>
+			  <SafeArewView style={{flex:1, backgroundColor:"#000"}} onLayout={onLayoutRootView}>
+				  <InitialLayout />
+				</SafeAreaView>
+			</SafeAreaProvider>
+			<StatusBar style="light" />
+		</ClerkAndConvexProvider>
 	);
 };

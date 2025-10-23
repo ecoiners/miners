8fe0c853f71565c85cc78e965a0f77c08fd7896d
ecoiners <damuna63@gmail.com>
2025-10-23 20:05:18 +0700

    web1

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { client } from './client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { client } from "./client";
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ThirdwebProvider } from 'thirdweb/react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThirdwebProvider } from "thirdweb/react";
+import { holesky } from "thirdweb/chains"; // ✅ IMPORT CHAIN OBJECT
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThirdwebProvider client={client} activeChain="holesky">
+    <ThirdwebProvider client={client} activeChain={holesky}>
       <Router>
-        <div className="min-h-screen bg-black">
+        <div className="min-h-screen bg-black text-white">
           <App />
         </div>
       </Router>
     </ThirdwebProvider>
-  </StrictMode>,
+  </StrictMode>
 );

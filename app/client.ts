53be53a0203e6a4449ecdc776f693f1547d6a154
ecoiners <damuna63@gmail.com>
2025-10-24 00:22:58 +0700

    updated

--- app/client.ts
+++ app/client.ts
@@ -1,12 +1,9 @@
 import { createThirdwebClient } from "thirdweb";
 
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.THIRDWEB_CLIENT_ID;
-
 export const client = createThirdwebClient({
-  clientId: clientId,
+  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!,
 });
 
-export const TokenAddress = process.env.TOKEN_ADDRESS || "0x4c3d7D3c4FE5c76E1d54f4dDd76EcCfdD1CA01df";
-export const TokenSaleAddress = process.env.TOKEN_SALE_ADDRESS || "0x48FEEC9694D1cFFd052662a417A14af2d9979d71";
+export const TokenAddress = process.env.NEXT_PUBLIC_TOKEN_ADDRESS || "0x4c3d7D3c4FE5c76E1d54f4dDd76EcCfdD1CA01df";
+export const TokenSaleAddress = process.env.NEXT_PUBLIC_TOKEN_SALE_ADDRESS || "0x48FEEC9694D1cFFd052662a417A14af2d9979d71";
+

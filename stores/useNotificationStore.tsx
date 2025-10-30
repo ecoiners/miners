224abc62832f59c3d958da39bac7ee6f9c6c6c3b
ecoiners <damuna63@gmail.com>
2025-10-31 02:04:27 +0700

    update 1

--- stores/useNotificationStore.tsx
+++ stores/useNotificationStore.tsx
@@ -1,20 +1,19 @@
-import { create, State } from "zustand";
+import create, { State } from "zustand";
 import produce from "immer";
 
 interface NotificationStore extends State {
-	notifications: Array<{
-		type: string;
-		message: string;
-		description?: string;
-		txid?: string;
-	}>;
-	set: (x: any) => void;
-	
+  notifications: Array<{
+    type: string;
+    message: string;
+    description?: string;
+    txid?: string;
+  }>;
+  set: (x: any) => void;
 }
 
 const useNotificationStore = create<NotificationStore>((set, _get) => ({
-	notifications: [],
-	set: (fn) => set(produce(fn))
+  notifications: [],
+  set: (fn) => set(produce(fn))
 }));
 
 export default useNotificationStore;

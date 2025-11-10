681b9e859984a401607bc4b2fc3a93fd400fdbff
ecoiners <damuna63@gmail.com>
2025-11-10 10:08:18 +0700

    upds

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
+import {
+  Loader,
+  NavBar,
+  Sidebar,
+  Hero,
+  About,
+  TechStack,
+  Projects,
+  Testimonials,
+  Footer,
+  Contact,
+} from "./sections";
 
-import { Routes, Route } from "react-router-dom";
-
-import HomePage from "./pages/home";
-import LoginPage from './pages/login';
-import RegisterPage from './pages/register';
-import VerifyEmailPage from "./pages/email-verify";
-import BuyPage from "./pages/buy";
-import ResetPasswordPage from "./pages/reset-password";
-import ForgotPasswordPage from "./pages/forgot-password";
-import Header from "./components/header"
-
-function App() {
-	
-	return (
-		<>
-		  <Header />
-			<Routes>
-			  <Route path="/" element={<HomePage />} />
-				<Route path="/login" element={<LoginPage/>}/>
-				<Route path="/register" element={<RegisterPage />} />
-				<Route path="/email-verify" element={<VerifyEmailPage/>} />
-				<Route path="/buy" element={<BuyPage/>} />
-				
-				<Route path="/forgot-password" element={<ForgotPasswordPage />}/>
-				<Route path="/reset-password" element={<ResetPasswordPage />} />
-			</Routes>
-		</>
-	)
+const App = () => {
+  return (
+    <div className="bg-black-100">
+      <Loader />
+      <NavBar />
+      <Sidebar />
+      <Hero />
+      <About />
+      <TechStack />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
